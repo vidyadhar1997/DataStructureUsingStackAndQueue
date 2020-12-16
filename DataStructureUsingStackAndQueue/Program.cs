c5b9a9eb0d2009d733a221f56fd20a83446e4fc2
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-16 13:21:32 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ DataStructureUsingStackAndQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureUsingStackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the stack And Queue program");
+        }
+    }
+}
